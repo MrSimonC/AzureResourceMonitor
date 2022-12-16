36ee430d3622347c8942ac66c3f61eb5e31d3616
Mr Simon C <iamontheinternet@yahoo.com>
2022-12-16 10:51:26 +0000

    debug connection string

--- TableStorageApi/Program.cs
+++ TableStorageApi/Program.cs
@@ -10,6 +10,7 @@ app.UseApiKeyAuthentication();
 app.Urls.Add("http://0.0.0.0:8080");
 
 var tableConnString = Environment.GetEnvironmentVariable("TABLESTORAGECONNECTIONSTRING");
+app.Logger.LogInformation($"got conection string {tableConnString}");
 var tableClient = new TableClient(tableConnString, tableName);
 
 app.MapGet("/liveness", () => "All OK");
