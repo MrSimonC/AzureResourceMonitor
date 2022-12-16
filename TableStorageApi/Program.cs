4c9a2764aef8c19ed80f14c2555afc3fb8db73f2
Mr Simon C <iamontheinternet@yahoo.com>
2022-12-16 11:01:00 +0000

    remove extra } mistake

--- TableStorageApi/Program.cs
+++ TableStorageApi/Program.cs
@@ -10,7 +10,6 @@ app.UseApiKeyAuthentication();
 app.Urls.Add("http://0.0.0.0:8080");
 
 var tableConnString = Environment.GetEnvironmentVariable("TABLESTORAGECONNECTIONSTRING");
-app.Logger.LogInformation($"got conection string {tableConnString}");
 var tableClient = new TableClient(tableConnString, tableName);
 
 app.MapGet("/liveness", () => "All OK");
